9ae38f16d3ba944cb7e3fac243cf8033655ef361
tth <1522986750@qq.com>
2020-07-06 22:11:35 +0800

    init

---
+++ mybatis/src/test/java/com/imooc/mybatis/MyBatisTestor.java
@@ -0,0 +1,44 @@
+package com.imooc.mybatis;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.net.ConnectException;
+import java.sql.Connection;
+
+//单元测试类
+public class MyBatisTestor {
+    @Test
+    public void testSqlSessionFactory() throws IOException {
+        //利用Reader加载classpath下的mybatis-config.xml核心配置文件
+        Reader reader = Resources.getResourceAsReader("mybatis-config.xml");
+        //初始化SqlSessionFactory对象，同时解析mybatis-config.xml文件
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+        System.out.println("SessionFactory加载成功");
+        //创建SqlSession对象,SqlSession是JDBC的扩展类，用于数据湖库交互
+        SqlSession sqlSession = null;
+        try {
+            sqlSessionFactory.openSession();
+            //创建数据库连接Connection仅用作测试用;
+            // 使用Mybatis会自动创建，无需显示创建;
+            // 尽量做到不引入jdbc相关类，如java.sql.connection;
+            Connection connection = sqlSession.getConnection();
+            System.out.println(connection);
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally{
+            if(sqlSession != null){
+                //在mybatis-config.xml中
+                //如果type=“POOLED”，代表使用连接池，close则是将链接回收到连接池中
+                //如果type=“UNPOOLED”，代表直连，close则会调用Connection.close()方法关闭连接
+                sqlSession.close();
+            }
+        }
+
+    }
+}
