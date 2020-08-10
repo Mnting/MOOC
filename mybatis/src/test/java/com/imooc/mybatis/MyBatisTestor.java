02b9a8108c15b17ca39d1b917cdd0d15477b7c38
Mnting <taotianhao97@qq.com>
2020-08-10 17:49:47 +0800

    202008101749

--- mybatis/src/test/java/com/imooc/mybatis/MyBatisTestor.java
+++ mybatis/src/test/java/com/imooc/mybatis/MyBatisTestor.java
@@ -1,5 +1,7 @@
 package com.imooc.mybatis;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.imooc.mybatis.dto.GoodsDTO;
 import com.imooc.mybatis.entity.Goods;
 import com.imooc.mybatis.entity.GoodsDetail;
@@ -313,6 +315,31 @@ public class MyBatisTestor {
             for (GoodsDetail goodsDetail : list) {
                 System.out.println(goodsDetail.getGdPicUrl() + ":" + goodsDetail.getGoods().getTitle());
             }
+        }catch (Exception e){
+            throw e;
+        }finally {
+            MyBatisUtils.closeSession(sqlSession);
+        }
+    }
+
+    @Test
+    public void testSelectPage() throws Exception {
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = MyBatisUtils.openSession();
+            /* startPage方法会自动将下一次查询进行分页 */
+            PageHelper.startPage(3,10);
+            Page<Goods> page = (Page)sqlSession.selectList("goods.selectPage");
+            System.out.println("总页数" + ":" + page.getPages());
+            System.out.println("总记录数" + ":" + page.getTotal());
+            System.out.println("开始行号" + ":" + page.getStartRow());
+            System.out.println("结束行号" + ":" + page.getEndRow());
+            System.out.println("当前页码" + ":" + page.getPageNum());
+            List<Goods> data = page.getResult();//当前页数据
+            for(Goods goods : data) {
+                System.out.println(goods.getTitle());
+            }
+            System.out.println("");
         }catch (Exception e){
             throw e;
         }finally {
