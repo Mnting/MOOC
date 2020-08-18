8751b78c77c441619e4d0aeeff9553c932adc906
mnting <taotianhao97@qq.com>
2020-08-19 00:52:34 +0800

    20200819

--- mybatis/src/test/java/com/imooc/mybatis/MyBatisTestor.java
+++ mybatis/src/test/java/com/imooc/mybatis/MyBatisTestor.java
@@ -352,7 +352,27 @@ public class MyBatisTestor {
             sqlSession = MyBatisUtils.openSession();
             List list = new ArrayList();
             long st = new Date().getTime();
+            for(int i = 0;i<10000;i++){
+                Goods goods = new Goods();
+                goods.setTitle("测试商品");
+                goods.setSubTitle("测试子标题");
+                goods.setOriginalCost(200f);
+                goods.setCurrentPrice(100f);
+                goods.setDiscount(0.5f);
+                goods.setIsFreeDelivery(1);
+                goods.setCategoryId(43);
+                //insert()方法返回值代表本次成功插入的记录
+
+                list.add(goods);
+            }
+            sqlSession.insert("goods.batchInsert",list);
+            sqlSession.commit();//提交事务数据
+            long et = new Date().getTime();
+            System.out.println("执行时间" + (et-st) + "毫秒");
         }catch (Exception e){
+            if(sqlSession != null){
+                sqlSession.rollback();//回滚事务
+            }
             throw e;
         }finally {
             MyBatisUtils.closeSession(sqlSession);
