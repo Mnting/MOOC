86c21650a7d4f17146b376b4ea7a94bb0012fc64
Mnting <taotianhao97@qq.com>
2020-08-28 17:23:16 +0800

    202008281638

---
+++ leetcode/src/InvertBinaryTree226.java
@@ -0,0 +1,7 @@
+/**
+ * @Author Tianhao Tao
+ * @Date 2020/8/28 17:17
+ * @Version 1.0
+ */
+public class InvertBinaryTree226 {
+}
--- leetcode/src/MaximumDepthofBinaryTree104.java
+++ leetcode/src/MaximumDepthofBinaryTree104.java
@@ -4,5 +4,13 @@
  * @Version 1.0
  */
 public class MaximumDepthofBinaryTree104 {
-    public int depth(TreeNode treeNode)
+    public int maxDepth(TreeNode root) {
+        return depth(root);
+    }
+    public int depth(TreeNode treeNode){
+        if (treeNode==null){return 0;}
+        int l = depth(treeNode.left);
+        int r = depth(treeNode.right);
+        return Math.max(l,r)+1;
+    }
 }
