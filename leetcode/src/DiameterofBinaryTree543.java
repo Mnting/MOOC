724c53e62d0e28037583251487a34638f3304dd5
Mnting <taotianhao97@qq.com>
2020-08-28 16:38:56 +0800

    202008281638

---
+++ leetcode/src/DiameterofBinaryTree543.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+import sun.reflect.generics.tree.Tree;
+
+import javax.swing.tree.TreeNode;
+import java.util.Enumeration;
+
+/**
+ * @Author Tianhao Tao
+ * @Date 2020/8/28 15:37
+ * @Version 1.0
+ */
+public class DiameterofBinaryTree543 {
+    int len = 1;
+    public int diameterOfBinaryTree(TreeNode node) {
+        length(node);
+        return len-1;
+    }
+
+    public int length(TreeNode node){
+        if(node==null){return 0;}
+        int l = length(node.left);
+        int r = length(node.right);
+        len = Math.max(len, l+r+1);
+        return Math.max(l, r)+1;
+    }
+}
