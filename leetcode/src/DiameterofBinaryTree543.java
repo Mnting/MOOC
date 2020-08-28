c27ecdbbf5633730db38201276d2fd057cdf95c1
Mnting <taotianhao97@qq.com>
2020-08-28 16:44:50 +0800

    202008281638

--- leetcode/src/DiameterofBinaryTree543.java
+++ leetcode/src/DiameterofBinaryTree543.java
@@ -10,17 +10,17 @@ import java.util.Enumeration;
  * @Version 1.0
  */
 public class DiameterofBinaryTree543 {
-    int len = 1;
+    int len = 0;
     public int diameterOfBinaryTree(TreeNode node) {
         length(node);
-        return len-1;
+        return len;
     }
 
     public int length(TreeNode node){
         if(node==null){return 0;}
         int l = length(node.left);
         int r = length(node.right);
-        len = Math.max(len, l+r+1);
+        len = Math.max(len, l+r);
         return Math.max(l, r)+1;
     }
 }
