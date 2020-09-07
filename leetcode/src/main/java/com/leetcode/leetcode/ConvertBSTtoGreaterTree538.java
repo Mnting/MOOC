62aafd4d353d420f95351b7628016d394f6bbe88
mnting <taotianhao97@qq.com>
2020-09-08 00:28:01 +0800

    20200906

---
+++ leetcode/src/main/java/com/leetcode/leetcode/ConvertBSTtoGreaterTree538.java
@@ -0,0 +1,9 @@
+package main.java.com.leetcode.leetcode;
+
+public class ConvertBSTtoGreaterTree538 {
+    public TreeNode convertBST(TreeNode root) {
+        if (root==null) return null;
+
+        return root;
+    }
+}
