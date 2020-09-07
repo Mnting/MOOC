7d8664a0b85999f32758530ff25c27270a928348
Mnting <taotianhao97@qq.com>
2020-09-07 18:35:22 +0800

    202009071835
    Signed-off-by: Mnting <taotianhao97@qq.com>

---
+++ imooc-oa/src/main/java/com/imooc/oa/controller/indexServlet.java
@@ -0,0 +1,35 @@
+package com.imooc.oa.controller;
+
+import com.imooc.oa.entity.Node;
+import com.imooc.oa.entity.User;
+import com.imooc.oa.service.UserService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @Author Tianhao Tao
+ * @Date 2020/9/7 17:09
+ * @Version 1.0
+ */
+@WebServlet(name = "indexServlet",urlPatterns = "/index")
+public class indexServlet extends HttpServlet {
+    private UserService userService = new UserService();
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("login_user");
+        List<Node> nodeList = userService.selectNodeByUserId(user.getUserId());
+        request.setAttribute("node_list", nodeList);
+        request.getRequestDispatcher("/index.ftl").forward(request, response);
+    }
+}
