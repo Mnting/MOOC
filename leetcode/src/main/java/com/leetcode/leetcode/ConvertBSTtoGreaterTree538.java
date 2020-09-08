a21a66e8c2a9df481d84914c4a5bb40014d08955
Mnting <taotianhao97@qq.com>
2020-09-08 18:01:22 +0800

    202008281638

--- leetcode/src/main/java/com/leetcode/leetcode/ConvertBSTtoGreaterTree538.java
+++ leetcode/src/main/java/com/leetcode/leetcode/ConvertBSTtoGreaterTree538.java
@@ -2,8 +2,21 @@ package main.java.com.leetcode.leetcode;
 
 public class ConvertBSTtoGreaterTree538 {
     public TreeNode convertBST(TreeNode root) {
-        if (root==null) return null;
-
+        if (root==null) {
+            return null;
+        }
+        if ((root!=null)&&(root.right.val<root.val)){
+            root.right.val+=root.val;
+        }
+        TreeNode node = convertBST(root.right);
+        if (node!=null){
+            int val = node.val;
+            root.val+=val;
+        }
+        if (root.left!=null){
+            root.left.val+=root.val;
+        }
+        convertBST(root.left);
         return root;
     }
 }
