e62f213eae373d0dc60fcde5022596ddd16b5727
mnting <taotianhao97@qq.com>
2020-09-09 00:59:08 +0800

    202009090035

--- leetcode/src/main/java/com/leetcode/leetcode/SymmetricTree101.java
+++ leetcode/src/main/java/com/leetcode/leetcode/SymmetricTree101.java
@@ -1,4 +1,25 @@
 package main.java.com.leetcode.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SymmetricTree101 {
+    private List<Integer> list = new ArrayList();
+    public boolean isSymmetric(TreeNode root) {
+        addList(root);
+        for(int i = 0;i<list.size()/2;i++){
+            if (list.get(i) != list.get(list.size()-i-1)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    TreeNode addList(TreeNode root){
+        if(root==null) {list.add(0);return null;}
+        addList(root.left);
+        list.add(root.val);
+        addList(root.right);
+        return root;
+    }
 }
