6ffc0f1a17418ec05a9d3286102edce38787851f
Mnting <taotianhao97@qq.com>
2020-10-15 18:03:42 +0800

    20201015

---
+++ S01/src/main/java/com/imooc/spring/ioc/Application.java
@@ -0,0 +1,23 @@
+package com.imooc.spring.ioc;
+
+import com.imooc.spring.ioc.entity.Apple;
+import com.imooc.spring.ioc.entity.Child;
+
+/**
+ * @Author Tianhao Tao
+ * @Date 2020/10/15 15:13
+ * @Version 1.0
+ */
+public class Application {
+    public static void main(String[] args) {
+        Apple apple1 = new Apple("红富士","red","Europe");
+        Apple apple2 = new Apple("青苹果","green","Asia");
+        Apple apple3 = new Apple("黄元帅","yellow","China");
+        Child lily = new Child("莉莉",apple1);
+        Child andy = new Child("安迪",apple2);
+        Child luna = new Child("露娜",apple3);
+        lily.eat();
+        andy.eat();
+        luna.eat();
+    }
+}
---
+++ S01/src/main/java/com/imooc/spring/ioc/entity/Apple.java
@@ -0,0 +1,45 @@
+package com.imooc.spring.ioc.entity;
+
+/**
+ * @Author Tianhao Tao
+ * @Date 2020/10/15 15:07
+ * @Version 1.0
+ */
+public class Apple {
+    private String title;
+    private String color;
+    private String origin;
+
+    public Apple() {
+    }
+
+    public Apple(String title, String color, String origin) {
+        this.title = title;
+        this.color = color;
+        this.origin = origin;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+}
