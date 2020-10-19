f2af777b1c2a01afcaaa58ca23c294746a3bf859
Mnting <taotianhao97@qq.com>
2020-10-19 17:56:41 +0800

    20201019

--- S01/src/main/java/com/imooc/spring/ioc02/factory/AppleFactoryInstance.java
+++ S01/src/main/java/com/imooc/spring/ioc02/factory/AppleFactoryInstance.java
@@ -6,10 +6,18 @@ package com.imooc.spring.ioc02.factory;
  * @Version 1.0
  */
 
+import com.imooc.spring.ioc02.entity.Apple;
+
 /**
  * 工厂实例方法创建对象是指IoC容器对工厂类进行实例化
  * 并调用对应的实例方法创建对象的过程
  */
 public class AppleFactoryInstance {
-
+    public Apple createSweetApple(){
+        Apple apple = new Apple();
+        apple.setColor("red");
+        apple.setTitle("红富士");
+        apple.setOrigin("欧洲");
+        return apple;
+    }
 }
