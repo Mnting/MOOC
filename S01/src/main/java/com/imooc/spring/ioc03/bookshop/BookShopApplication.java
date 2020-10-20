e4028a9fe3df0c994c54b592d4649537cf1f51e7
mnting <taotianhao97@qq.com>
2020-10-21 01:46:27 +0800

    202010210042

---
+++ S01/src/main/java/com/imooc/spring/ioc03/bookshop/BookShopApplication.java
@@ -0,0 +1,18 @@
+package com.imooc.spring.ioc03.bookshop;
+
+import com.imooc.spring.ioc03.bookshop.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @Author Tianhao Tao
+ * @Date 2020/10/20 21:31
+ * @Version 1.0
+ */
+public class BookShopApplication {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext03-*.xml");
+        BookService bookService = context.getBean("bookService",BookService.class);
+        bookService.purchase();
+    }
+}
