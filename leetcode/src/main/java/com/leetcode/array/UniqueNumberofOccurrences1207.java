31ec592cd91497878cdede946f65450df4925a4c
Mnting <taotianhao97@qq.com>
2020-10-28 15:17:12 +0800

    20201028leetcode1207

--- leetcode/src/main/java/com/leetcode/array/UniqueNumberofOccurrences1207.java
+++ leetcode/src/main/java/com/leetcode/array/UniqueNumberofOccurrences1207.java
@@ -3,17 +3,15 @@ package main.java.com.leetcode.array;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
-
-/**
- * @Author Tianhao Tao
- * @Date 2020/10/28 15:13
- * @Version 1.0
- */
-
 /**
  * 这个题是抄的大神题解@宝石
  * 对HashMap方法以及HashSet原理的的熟练运用
  * 值得学习~
+ */
+/**
+ * @author Tianhao Tao
+ * @Date 2020/10/28 15:13
+ * @Version 1.0
  */
 public class UniqueNumberofOccurrences1207 {
     public boolean uniqueOccurrences(int[] arr) {
