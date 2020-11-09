e7e8a23427dc4a5ec65f4ab1b8a8b2bdff1215f5
Mnting <taotianhao97@qq.com>
2020-11-09 10:02:51 +0800

    20201109

---
+++ gitshow/src/main/java/top/taotianhao/gitshow/GitshowApplication.java
@@ -0,0 +1,13 @@
+package top.taotianhao.gitshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitshowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitshowApplication.class, args);
+    }
+
+}
