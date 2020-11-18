4f1c4ce62946660cdaa61b434bb923e683f3fff0
Mnting <taotianhao97@qq.com>
2020-11-18 17:33:52 +0800

    20201118

---
+++ gitshow/src/main/java/top/taotianhao/gitshow/controller/GraphContoller.java
@@ -0,0 +1,15 @@
+package top.taotianhao.gitshow.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @Author Tianhao Tao
+ * @Date 2020/11/18 15:32
+ * @Version 1.0
+ */
+@Controller
+public class GraphContoller {
+    @GetMapping("/graph")
+    public String graph(){return "graph";}
+}
---
+++ gitshow/src/main/resources/static/js/graph.js
@@ -0,0 +1,163 @@
+import G6 from '@antv/g6';
+
+G6.registerNode('card-node', {
+    draw: function drawShape(cfg, group) {
+        const r = 2;
+        const color = '#5B8FF9';
+        const w = cfg.size[0];
+        const h = cfg.size[1];
+        const shape = group.addShape('rect', {
+            attrs: {
+                x: -w / 2,
+                y: -h / 2,
+                width: w, //200,
+                height: h, // 60
+                stroke: color,
+                radius: r,
+                fill: '#fff',
+            },
+            name: 'main-box',
+            draggable: true,
+        });
+
+        group.addShape('rect', {
+            attrs: {
+                x: -w / 2,
+                y: -h / 2,
+                width: w, //200,
+                height: h / 2, // 60
+                fill: color,
+                radius: [r, r, 0, 0],
+            },
+            name: 'title-box',
+            draggable: true,
+        });
+
+        // title text
+        group.addShape('text', {
+            attrs: {
+                textBaseline: 'top',
+                x: -w / 2 + 8,
+                y: -h / 2 + 2,
+                lineHeight: 20,
+                text: cfg.id,
+                fill: '#fff',
+            },
+            name: 'title',
+        });
+        cfg.children &&
+        group.addShape('marker', {
+            attrs: {
+                x: w / 2,
+                y: 0,
+                r: 6,
+                cursor: 'pointer',
+                symbol: G6.Marker.collapse,
+                stroke: '#666',
+                lineWidth: 1,
+                fill: '#fff',
+            },
+            name: 'collapse-icon',
+        });
+        group.addShape('text', {
+            attrs: {
+                textBaseline: 'top',
+                x: -w / 2 + 8,
+                y: -h / 2 + 24,
+                lineHeight: 20,
+                text: 'description',
+                fill: 'rgba(0,0,0, 1)',
+            },
+            name: `description`,
+        });
+        return shape;
+    },
+    setState(name, value, item) {
+        if (name === 'collapsed') {
+            const marker = item.get('group').find((ele) => ele.get('name') === 'collapse-icon');
+            const icon = value ? G6.Marker.expand : G6.Marker.collapse;
+            marker.attr('symbol', icon);
+        }
+    },
+});
+
+const data = {
+    id: 'A',
+    children: [
+        {
+            id: 'A1',
+            children: [{ id: 'A11' }, { id: 'A12' }, { id: 'A13' }, { id: 'A14' }],
+        },
+        {
+            id: 'A2',
+            children: [
+                {
+                    id: 'A21',
+                    children: [{ id: 'A211' }, { id: 'A212' }],
+                },
+                {
+                    id: 'A22',
+                },
+            ],
+        },
+    ],
+};
+
+const width = document.getElementById('container').scrollWidth;
+const height = document.getElementById('container').scrollHeight || 500;
+
+const graph = new G6.TreeGraph({
+    container: 'container',
+    width,
+    height,
+    defaultNode: {
+        type: 'card-node',
+        size: [100, 40],
+    },
+    defaultEdge: {
+        type: 'cubic-horizontal',
+        style: {
+            endArrow: true,
+        },
+    },
+    layout: {
+        type: 'indented',
+        direction: 'LR',
+        dropCap: false,
+        indent: 200,
+        getHeight: () => {
+            return 60;
+        },
+    },
+});
+
+// graph.data(data);
+// graph.render();
+// graph.fitView();
+// graph.on('node:click', (e) => {
+//     if (e.target.get('name') === 'collapse-icon') {
+//         e.item.getModel().collapsed = !e.item.getModel().collapsed;
+//         graph.setItemState(e.item, 'collapsed', e.item.getModel().collapsed);
+//         graph.layout();
+//     }
+// });
+//
+// <script src="https://gw.alipayobjects.com/os/antv/pkg/_antv.g6-3.1.0/build/g6.js"></script>
+//     <script>
+// const graph = new G6.Graph({
+//     container: 'mountNode',
+//     width: 1000,
+//     height: 600,
+//     renderer: 'svg',
+//     fitView: true,
+//     fitViewPadding: [ 20, 40, 50, 20 ]
+// });
+//
+// const main = async () => {
+//     const response = await fetch('https://gw.alipayobjects.com/os/basement_prod/6cae02ab-4c29-44b2-b1fd-4005688febcb.json');
+//     const remoteData = await response.json();
+//     graph.data(remoteData);
+//     graph.render();
+// };
+// main();
+// </script>
