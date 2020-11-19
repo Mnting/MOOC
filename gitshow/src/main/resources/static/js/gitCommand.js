43eaf1108e84ca535f22bfe99856e2bf6de1b752
mnting <taotianhao97@qq.com>
2020-11-20 01:30:46 +0800

    20201119

---
+++ gitshow/src/main/resources/static/js/gitCommand.js
@@ -0,0 +1,21 @@
+$(function () {
+    $("[data-toggle='popover']").popover({
+        trigger:'manual',
+        placement : 'bottom',//弹出位置
+        html : true,//content中插入html代码
+        animation: false
+    }).on("mouseenter", function () {
+        var _this = this;
+        $(this).popover("show");
+        $(this).siblings(".popover").on("mouseleave", function () {
+            $(_this).popover('hide');
+        });
+    }).on("mouseleave", function () {
+        var _this = this;
+        setTimeout(function () {
+            if (!$(".popover:hover").length) {
+                $(_this).popover("hide")
+            }
+        }, 100);
+    });
+});
